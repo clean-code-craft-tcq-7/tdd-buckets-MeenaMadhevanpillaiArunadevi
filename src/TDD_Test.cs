c3fd74e674756052a3c6d24a21468be4195547ee
MeenaMadhevanpillaiArunadevi <115541323+MeenaMadhevanpillaiArunadevi@users.noreply.github.com>
2023-01-30 15:34:10 +0530

    Update Test with Data-Driven Testing

--- src/TDD_Test.cs
+++ src/TDD_Test.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using Xunit;
 namespace TestProject1
 {
@@ -7,8 +9,9 @@ namespace TestProject1
         [Fact]
         public void Test1()
         {
+            //1.Basic Unit Testing
             TDD_ChargeRangeCalculator rangeDetector = new TDD_ChargeRangeCalculator();
-            Assert.True(rangeDetector.getChargingSession_Ranges(new int[]{4, 5}).Contains("4-5, 2"));
+            Assert.True(rangeDetector.getChargingSession_Ranges(new int[] { 4, 5 }).Contains("4-5, 2"));
 
             Assert.True(rangeDetector.getChargingSession_Ranges(new int[] { 3, 3 }).Contains("3, 2"));
 
@@ -25,5 +28,33 @@ namespace TestProject1
             Assert.True(rangeDetector.getChargingSession_Ranges(new int[] { 3, 4, 5, 10 }).Contains("3-5, 3"));
             Assert.True(rangeDetector.getChargingSession_Ranges(new int[] { 3, 4, 5, 10 }).Contains("10, 1"));
         }
+
+        //2. Data-Driven Unit Testing - for same use case
+        [Theory,ClassData(typeof(DataDrivenTest_Input))]
+        public void DataDrivenTest_RangeWithUniqueValues(int[] inputdata, string expectedOutput)
+        {
+            TDD_ChargeRangeCalculator rangeDetector = new TDD_ChargeRangeCalculator();
+            var actualResult = rangeDetector.getChargingSession_Ranges(inputdata);
+            Assert.Equal(expectedOutput, actualResult);
+
+        }
+    }
+    public class DataDrivenTest_Input : IEnumerable<object[]>
+    {
+           
+            public IEnumerator<object[]> GetEnumerator()
+            {
+                //>=1 range with unique values (sorted/unsorted) - use case 
+                //sorted
+                yield return new object[] { new int[] {3,5,7}, "3, 1\r\n5, 1\r\n7, 1\r\n" };
+                //unsorted
+                yield return new object[] { new int[] { 7, 6, 4, 5, 12, 15, 14 }, "4-7, 4\r\n12, 1\r\n14-15, 2\r\n" };
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return GetEnumerator();
+            }
     }
 }
+
